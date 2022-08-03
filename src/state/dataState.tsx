70feb685d1f408050c94c9d46454f4058829f747
dasom <50973005+dasom222g@users.noreply.github.com>
2022-08-04 00:00:16 +0900

    Refacotring

--- src/state/dataState.tsx
+++ src/state/dataState.tsx
@@ -31,8 +31,3 @@ export const solutionState = atom<string>({
   key: 'solutionState',
   default: initialSolution,
 })
-
-export const modalOpenState = atom<boolean>({
-  key: 'modalOpenState',
-  default: false,
-})
